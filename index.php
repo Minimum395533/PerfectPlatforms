fed1986ec4f99f736a650c675ac2af7105c25616
Minun61 <395533@guhsd.net>
2026-02-27 10:03:46 -0800

    index.php base

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <html>
   <head>
-    <title>PHP Test</title>
+    <title>Perfect Platforms</title>
   </head>
   <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-
+    <nav>
+      <a href="/">Perfect Platforms</a>
+      <a href="about.php">About me</a>
+    </nav>
+    <canvas id="myCanvas" width="500" height="300" style="border:1px solid #000;">
+      Upgrade your browser to support HTML5 canvas.
+    </canvas> 
+    <canvas id="leaderboard" width="100" height="300" style="border:1px solid #000;">
+      Upgrade your browser to support HTML5 canvas.
+    </canvas> 
+    <script src="gamescreen.js"></script>
+  </body>
 </html>
