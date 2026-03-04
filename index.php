eba55af53a0a9910fa49c66c31b365ce95beb2eb
Minun61 <395533@guhsd.net>
2026-03-04 11:02:42 -0800

    Update index.php
    added player.js as a resource

--- index.php
+++ index.php
@@ -79,6 +79,7 @@
     </div>
 
     <script src="gamescreen.js"></script>
+     <script src="player.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
